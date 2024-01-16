a0859c0fe18e9f6b2925940b944e8e8eedc897e1
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-01-16 16:26:17 +0800

    feat: update release with @actions/pkgs

--- src/command/release.ts
+++ src/command/release.ts
@@ -1,8 +1,8 @@
 import { promises as fsp } from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
-import { ofetch } from 'ofetch'
-import p from 'picocolors'
+import { getOctokit } from '@actions/github'
+import { info, setFailed, warning } from '@actions/core'
 
 const cwd = process.cwd()
 
@@ -17,59 +17,62 @@ export function resolveChangelogSection(content: string) {
   catch {
   }
 
-  console.log(p.yellow('Failed to resolve release notes on CHANGELOG.md. Callback to "".'))
+  warning('Failed to resolve release notes on CHANGELOG.md. Callback to "".')
 
   return ''
 }
 
-// https://github.com/antfu/changelogithub/blob/f6995c9cb4dda18a0fa21efe908a0ee6a1fc26b9/src/github.ts#L7
 export async function sendRelease() {
   const { CI, GITHUB_REPOSITORY: repository, GITHUB_REF_NAME: tag, GITHUB_TOKEN: token } = process.env
 
+  const [owner, repo] = repository!.split('/')
+
+  const octokit = getOctokit(token!)
+
   if (CI !== 'true') {
-    console.log(p.yellow('Not in CI env. Skip release.'))
+    warning('Not in CI env. Skip release.')
     return
   }
 
   if (!token) {
-    console.log(p.yellow('No token in env. Skip release.'))
+    warning('No token in env. Skip release.')
     return
   }
-
-  const headers = { accept: 'application/vnd.github+json', authorization: `token ${token}` }
-  let url = `https://api.github.com/repos/${repository}/releases`
-  let method = 'POST'
-
-  /* eslint-disable ts/no-unsafe-assignment, ts/no-unsafe-member-access */
-  try {
-    const exists = await ofetch(`https://api.github.com/repos/${repository}/releases/tags/${tag}`, { headers })
-    if (exists.url) {
-      url = exists.url
-      method = 'PATCH'
-    }
-  }
-  catch {}
-  /* eslint-enable ts/no-unsafe-assignment, ts/no-unsafe-member-access */
 
   const CHANGELOG_PATH = path.resolve(cwd, 'CHANGELOG.md')
   await fsp.stat(CHANGELOG_PATH)
   const changelogContent = await fsp.readFile(CHANGELOG_PATH, { encoding: 'utf-8' })
-
-  const body = {
+  const releaseBody = {
+    owner,
+    repo,
     body: resolveChangelogSection(changelogContent),
-    name: tag,
-    tag_name: tag,
+    name: tag!,
+    tag_name: tag!,
   }
 
-  console.log(p.cyan(method === 'POST' ? 'Creating a release...' : 'Updating the release notes...'))
+  const createRes = await octokit.rest.repos.createRelease(releaseBody)
 
-  /* eslint-disable ts/no-unsafe-assignment, ts/no-unsafe-member-access */
-  const res = await ofetch(url, {
-    method,
-    body: JSON.stringify(body),
-    headers,
-  })
+  if (createRes.status !== 201) {
+    // @ts-expect-error Validation Failed
+    // eslint-disable-next-line ts/no-unsafe-member-access
+    if (createRes.data.errors[0].code === 'already_exists') {
+      info('The `tag_name` of release already exists. So update it.`')
+      const { data: { id } } = await octokit.rest.repos.getReleaseByTag({
+        owner,
+        repo,
+        tag: tag!,
+      })
 
-  console.log(p.green(`Released on ${res.html_url}`))
-  /* eslint-enable ts/no-unsafe-assignment, ts/no-unsafe-member-access */
+      await octokit.rest.repos.updateRelease({
+        release_id: id,
+        ...releaseBody,
+      })
+      info('Successfully updated a release.')
+    }
+    else {
+      info('Successfully created a release.')
+    }
+
+    setFailed(`Fail with ${JSON.stringify(createRes.data)}`)
+  }
 }
