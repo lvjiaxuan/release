40b7ca0b3e8a77e950c4b0b188f2bb4227b2814b
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-01-17 18:17:57 +0800

    feat: log publish command.

--- src/command/publish.ts
+++ src/command/publish.ts
@@ -2,7 +2,8 @@ import process from 'node:process'
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { $ } from 'execa'
-import { setFailed } from '@actions/core'
+import { info } from '@actions/core'
+import p from 'picocolors'
 import type { PublishOption } from '..'
 
 const cwd = process.cwd()
@@ -10,6 +11,7 @@ const cwd = process.cwd()
 export async function publish(options: PublishOption) {
   const $$ = $({ stdio: 'inherit' })
 
+  info(p.blue('pnpm publish -r --report-summary --no-git-checks'))
   await $$`pnpm publish -r --report-summary --no-git-checks`
 
   if (options.syncCnpm) {
@@ -23,7 +25,9 @@ export async function publish(options: PublishOption) {
 
     const publishedNames = summaryJson.publishedPackages.map(item => item.name)
     if (publishedNames.length) {
+      info(p.blue('pnpm i cnpm -g'))
       await $$`pnpm i cnpm -g`
+      info(p.blue(`cnpm sync ${publishedNames.join(' ')} --sync-publish`))
       await $$`cnpm sync ${publishedNames.join(' ')} --sync-publish`
     }
   }
