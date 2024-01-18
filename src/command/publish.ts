4b89c1aeeb314d8bc1ec9e381db0816f6c4de891
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-01-18 15:03:19 +0800

    feat: separate pnpm workspace

--- src/command/publish.ts
+++ src/command/publish.ts
@@ -1,34 +1,58 @@
 import process from 'node:process'
 import path from 'node:path'
-import fs from 'node:fs/promises'
-import { $ } from 'execa'
-import { info } from '@actions/core'
+import fsp from 'node:fs/promises'
 import p from 'picocolors'
+import { info } from '@actions/core'
+import { $ } from 'execa'
 import type { PublishOption } from '..'
 
-const cwd = process.cwd()
-
 export async function publish(options: PublishOption) {
+  const cwd = process.cwd()
+
   const $$ = $({ stdio: 'inherit' })
 
-  info(p.blue('pnpm publish -r --report-summary --no-git-checks'))
-  await $$`pnpm publish -r --report-summary --no-git-checks`
+  const isWorkspace = await (async () => {
+    try {
+      await fsp.access(path.join(cwd, 'pnpm-workspace.yaml'), fsp.constants.F_OK)
+      return true
+    }
+    catch {}
+    return false
+  })()
+
+  const publishCommand = `pnpm publish${isWorkspace ? ' -r --report-summary' : ''} --no-git-checks`
+  info(`${p.blue(publishCommand)}\n`)
+  await $$`${publishCommand}`
 
   if (options.syncCnpm) {
-    const summaryStr = await fs.readFile(path.join(cwd, 'pnpm-publish-summary.json'))
-    const summaryJson = JSON.parse(summaryStr.toString()) as {
-      publishedPackages: {
-        name: string
-        version: string
-      }[]
+    let publishedNames: string[] = []
+
+    if (isWorkspace) {
+      const summaryJson = JSON.parse(
+        await fsp.readFile(path.join(cwd, 'pnpm-publish-summary.json'), 'utf-8'),
+      ) as {
+        publishedPackages: {
+          name: string
+          version: string
+        }[]
+      }
+
+      publishedNames = summaryJson.publishedPackages.map(item => item.name)
+    }
+    else {
+      const pkgJson = JSON.parse(
+        await fsp.readFile(path.join(cwd, 'package.json'), 'utf-8'),
+      ) as { name: string }
+
+      publishedNames = [pkgJson.name]
     }
 
-    const publishedNames = summaryJson.publishedPackages.map(item => item.name)
-    if (publishedNames.length) {
-      info(p.blue('pnpm i cnpm -g'))
-      await $$`pnpm i cnpm -g`
-      info(p.blue(`cnpm sync ${publishedNames.join(' ')} --sync-publish`))
-      await $$`cnpm sync ${publishedNames.join(' ')} --sync-publish`
-    }
+    info(p.blue('pnpm i cnpm -g\n'))
+    await $$`pnpm i cnpm -g`
+
+    info(p.blue(`cnpm sync ${publishedNames.join(' ')} --sync-publish\n`))
+    await $$`cnpm sync ${publishedNames.join(' ')} --sync-publish`
   }
+
+  process.exit(0)
 }
